488dd8b15ce75965c03ca87563b9ecc491fa394b
ThoKhang <khangheheqt@gmail.com>
2024-10-15 20:28:05 +0700

    them day du SQL

--- BAITAPTUAN5_2_SQL/SQL_tuan5.sql
+++ BAITAPTUAN5_2_SQL/SQL_tuan5.sql
@@ -1,21 +1,70 @@
+create database Nhom7_Tuan5
+go
+use Nhom7_Tuan5
+go
 create table NHACUNGCAP
 (
 	MACONGTY char(10) primary key,
 	TENCONGTY nvarchar(50) not null,
-	TENGIAODICH varchar(50) null,
-	DIACHI varchar(50) not null,
-	DIENTHOAI varchar(10) not null,
-	FAX varchar(10) not null,
-	EMAIL varchar(50) not null
+	TENGIAODICH nvarchar(50) null,
+	DIACHI nvarchar(50) not null,
+	DIENTHOAI char(12) not null,
+	FAX char(10) not null,
+	EMAIL varchar(30) not null,
+)
+create table LOAIHANG 
+(
+	MALOAIHANG char(10) primary key,
+	TENLOAIHANG nvarchar (30) not null,
 )
 create table MATHANG
 (
 	MAHANG char(10) primary key,
-	TENHANG varchar(50) not null,
-	MACONGTY char(10) not null,
-	MALOAIHANG varchar(10) not null,
+	TENHANG nvarchar(30) not null,
+	NHACUNGCAPNO char(10) not null foreign key references NHACUNGCAP(MACONGTY),
+	LOAIHANGNO char(10) not null foreign key references LOAIHANG(MALOAIHANG),
 	SOLUONG int check(SOLUONG>=0),
 	DONVITINH varchar(20) not null,
 	GIAHANG money not null,
-	foreign key (MACONGTY) references NHACUNGCAP(MACONGTY)
+)
+create table KHACHHANG
+(
+	MAKHACHHANG char(10) primary key,
+	TENCONGTY nvarchar(30) not null,
+	TENGIAODICH nvarchar(30) not null,
+	DIACHI nvarchar(30) not null,
+	EMAIL nvarchar(30) not null,
+	DIENTHOAI char(11) not null,
+	FAX char (11) null,
+)
+create table NHANVIEN
+(
+	MANHANVIEN char(10) primary key,
+	HO nvarchar(50) not null,
+	TEN nvarchar(50) not null,
+	NGAYSINH date not null,
+	NGAYLAMVIEC date not null,
+	DIACHI nvarchar(100) not null,
+	DIENTHOAI char(15) not null,
+	LUONGCOBAN decimal(10,20) not null,
+	PHUCAP decimal(10,20) not null,
+)
+create table DONDATHANG
+(
+	SOHOADON char(10) primary key,
+	KHACHHANGNO char(10) not null foreign key references KHACHHANG(MAKHACHHANG),
+	MANHANVIEN char(10) not null foreign key references NHANVIEN(MANHANVIEN),
+	NGAYDATHANG date not null,
+	NGAYGIAOHANG date not null,
+	NGAYCHUYENHANG date not null,
+	NOIGIAOHANG nvarchar(100) not null,
+)
+create table CHITIETDATHANG
+(
+	DONDATHANGNO char(10) not null foreign key references DONDATHANG(SOHOADON),
+	MATHANGNO char(10) not null foreign key references MATHANG(MAHANG),
+	primary key(DONDATHANGNO,MATHANGNO),
+	GIABAN decimal(10,2) not null,
+	SOLUONG int not null,
+	MAGIAMGIA varchar(20) not null,
 )
